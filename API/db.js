36161369082baa824064aed6237ef070f082487e
wakenedo <a_nuner@outlook.com>
2025-04-02 19:15:02 -0300

    Env Added

--- API/db.js
+++ API/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const db = mysql.createConnection({
-  host: "servermysqlcn1.mysql.database.azure.com",
-  user: "userdb",
-  password: "admin@123",
-  database: "alexandre",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
